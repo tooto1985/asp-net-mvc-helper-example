50534562275dd6569e4b8b55cc33b8b62fe0c935
Tommy <tooto1985@gmail.com>
2017-07-21 14:14:17 +0800

    add

---
+++ WebApplication1/Helper/helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Helpers
+{
+    public static class hp
+    {
+        public static string ShowH1(this HtmlHelper helper,  string text) {
+
+            return "<h1>" + text + "</h1>";
+        }
+
+    }
+}
